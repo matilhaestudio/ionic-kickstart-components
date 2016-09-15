c86e343288a81bb86818defc1bda842505ab94d6
Marcelo Barreto <marcelo@matilhaestud.io>
2016-09-15 15:16:22 -0300

    register and login

--- app/components/auth/auth.module.js
+++ app/components/auth/auth.module.js
@@ -21,24 +21,24 @@ angular
       });
 
     firebase.initializeApp(config);
+  })
+  .run(function ($transitions, $state, AuthService) {
+    $transitions.onStart({
+      to: function (state) {
+        return !!(state.data && state.data.requiredAuth);
+      }
+    }, function() {
+      return AuthService
+        .requireAuthentication()
+        .catch(function () {
+          return $state.target('auth.login');
+        });
+    });
+    $transitions.onStart({
+      to: 'auth.*'
+    }, function () {
+      if (AuthService.isAuthenticated()) {
+        return $state.target('app');
+      }
+    });
   });
-  // .run(function ($transitions, $state, AuthService) {
-  //   $transitions.onStart({
-  //     to: function (state) {
-  //       return !!(state.data && state.data.requiredAuth);
-  //     }
-  //   }, function() {
-  //     return AuthService
-  //       .requireAuthentication()
-  //       .catch(function () {
-  //         return $state.target('auth.login');
-  //       });
-  //   });
-  //   $transitions.onStart({
-  //     to: 'auth.*'
-  //   }, function () {
-  //     if (AuthService.isAuthenticated()) {
-  //       return $state.target('app');
-  //     }
-  //   });
-  // });
